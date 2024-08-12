3508eb965eaa1373a7483daeeea98c17cb32ed00
Ümit Çakmak <umit.cakmak@altogic.com>
2024-08-12 12:49:21 +0300

    postgresql changes

--- platform/handlers/templates/index.js
+++ platform/handlers/templates/index.js
@@ -145,7 +145,7 @@ export const templates = [
 					variables: {
 						POSTGRES_USER: "{{secret:username}}",
 						POSTGRES_PASSWORD: "{{secret:password}}",
-						PGDATA: "/var/lib/postgresql/dstore",
+						PGDATA: "/var/lib/postgresql/dstore/data",
 					},
 				},
 			},
